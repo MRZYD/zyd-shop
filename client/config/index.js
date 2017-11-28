279afaf0a9d35b48dd0572f7e44598484999a87c
MRZYD <2416389097@qq.com>
2017-11-28 18:29:59 +0800

    add

--- client/config/index.js
+++ client/config/index.js
@@ -11,13 +11,13 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      "/users": {
-        target: "http://localhost:3000"
-      },
-      "/goods": {
-        target: "http://localhost:3000"
-      }
-    },
+            '/api/**': {
+                target: 'http://localhost:3000',
+                pathRewrite:{
+                    '^/api':'/'
+                }
+            },
+        },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
